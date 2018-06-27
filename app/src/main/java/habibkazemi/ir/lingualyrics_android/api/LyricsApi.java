253a5d32db86dd18cd2fd886ba45bb6d00f64ccf
Habib Kazemi <kazemihabib1996@gmail.com>
2018-06-27 18:10:45 +0430

    Fix url

--- app/src/main/java/habibkazemi/ir/lingualyrics_android/api/LyricsApi.java
+++ app/src/main/java/habibkazemi/ir/lingualyrics_android/api/LyricsApi.java
@@ -7,7 +7,7 @@ import android.content.Context;
  */
 
 public class LyricsApi {
-    public static final String BASE_URL = "http://lingualyrics.habibkazemi.ir";
+    public static final String BASE_URL = "http://lingualyrics.habibkazemi.ir/";
 //    public static final String BASE_URL = "http://10.0.2.2:8000/";
 
     public static LyricsService getLyricService(Context context){
